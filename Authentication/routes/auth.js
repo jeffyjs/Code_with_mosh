6c8484ca2f8bff08419aaceb12da1ccd28ec5374
jeffyjs <jefferson@karunya.edu.in>
2020-06-02 18:43:08 +0530

    Auth

---
+++ Authentication/routes/auth.js
@@ -0,0 +1,39 @@
+const config =require('config');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const {User} = require('../models/user')
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body); 
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    let user = await User.findOne({email:req.body.email});
+    if(!user) return res.status(400).send('Invalid Email or Password');
+
+    const validpassword = await bcrypt.compare(req.body.password, user.password);
+    if(!validpassword) return res.status(400).send('Invalid mail or password');
+    
+    const token = user.genrateAuthToken();
+    res.send(token);
+});
+
+
+function validate(req ) {
+    const schema = {
+   
+      email: Joi.string().min(5).max(255).required(),
+      password: Joi.string().min(5).max(255).required()
+    };
+  
+    return Joi.validate(req, schema);
+  }
+  
+ 
+module.exports = router;
+
